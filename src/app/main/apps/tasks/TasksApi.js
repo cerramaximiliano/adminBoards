cad8c8bd6beecdd1d1d9c6680b8d38d05d89a037
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-04-09 16:17:04 -0300

    fix/ git ignore

---
+++ src/app/main/apps/tasks/TasksApi.js
@@ -0,0 +1,102 @@
+import { apiService as api } from 'app/store/apiService';
+import { showMessage } from '@fuse/core/FuseMessage/fuseMessageSlice';
+
+export const addTagTypes = ['tasks_list', 'tasks_item', 'tasks_tags'];
+
+const TasksApi = api
+	.enhanceEndpoints({
+		addTagTypes
+	})
+	.injectEndpoints({
+		endpoints: (build) => ({
+			getTasks: build.query({
+				query: (baseUrl) => 
+					{
+						return {
+							url: `${baseUrl}tasks`,
+							}
+			},
+				providesTags: ['tasks_list']
+			
+			}),
+			reorderTasks: build.mutation({
+				query: ({ startIndex, endIndex }) => ({
+					url: `/mock-api/tasks/reorder`,
+					method: 'POST',
+					data: { startIndex, endIndex }
+				}),
+				invalidatesTags: ['tasks_list'],
+				async onQueryStarted(_, { dispatch, queryFulfilled }) {
+					try {
+						await queryFulfilled;
+						dispatch(
+							showMessage({
+								message: 'List Order Saved',
+								autoHideDuration: 2000,
+								anchorOrigin: {
+									vertical: 'top',
+									horizontal: 'right'
+								}
+							})
+						);
+					} catch (err) {
+						dispatch(showMessage({ message: 'Error saving list order' }));
+					}
+				}
+			}),
+			createTasksItem: build.mutation({
+				query: (task) => ({
+					url: `/mock-api/tasks`,
+					method: 'POST',
+					data: task
+				}),
+				invalidatesTags: ['tasks_list']
+			}),
+			getTasksItem: build.query({
+				query: (baseUrl) => ({
+ 					url: `${baseUrl}`,
+				})
+					,
+				providesTags: ['tasks_item']
+			}),
+			deleteTasksItem: build.mutation({
+				query: (taskId) => ({
+					url: `/mock-api/tasks/${taskId}`,
+					method: 'DELETE'
+				}),
+				invalidatesTags: ['tasks_list']
+			}),
+			updateTasksItem: build.mutation({
+				query: (task) => ({
+					url: `/mock-api/tasks/${task.id}`,
+					method: 'PUT',
+					data: task
+				}),
+				invalidatesTags: ['tasks_item', 'tasks_list']
+			}),
+			getTasksTags: build.query({
+				query: (baseUrl) => ({ url: `/mock-api/tasks/tags` }),
+				providesTags: ['tasks_tags']
+			}),
+			createTasksTag: build.mutation({
+				query: (tag) => ({
+					url: `/mock-api/tasks/tags`,
+					method: 'POST',
+					data: tag
+				}),
+				invalidatesTags: ['tasks_tags']
+			})
+		}),
+		overrideExisting: false
+	});
+export { TasksApi };
+export const {
+	useGetTasksQuery,
+	useCreateTasksItemMutation,
+	useGetTasksItemQuery,
+	useDeleteTasksItemMutation,
+	useUpdateTasksItemMutation,
+	useGetTasksTagsQuery,
+	useCreateTasksTagMutation,
+	useReorderTasksMutation
+} = TasksApi;
---
+++ src/app/store/apiService.js
@@ -0,0 +1,30 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import Axios from 'axios';
+
+const axiosBaseQuery = () => async ({ url, method, data, params }) => {
+	Axios.defaults.baseURL = '/api';
+    try {
+        const result = await Axios({
+            url,
+            method,
+            data,
+            params,
+            headers: {
+                'x-api-key': 'WMEyVRFmNegvwuB1YT4rkKW5Yjh1'
+            }
+        });
+        console.log(result.data, url)
+        return { data: result.data };
+    } catch (axiosError) {
+        const error = axiosError;
+        return { error };
+    }
+};
+
+
+export const apiService = createApi({
+	baseQuery: axiosBaseQuery(),
+	endpoints: () => ({}),
+	reducerPath: 'apiService'
+});
+export default apiService;
